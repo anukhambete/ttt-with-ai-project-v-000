ff346b22cfb80a280966131155fcc3780952eb7c
Anu Khambete <anukhambete@gmail.com>
2018-05-21 23:40:57 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,6 +3,7 @@ module Players
     
     def move(board)
     #puts "Which cell would you like to pick?"
+    if @board.cells.all?
       input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
     end
     
