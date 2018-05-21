2de7588320d9b00f68a1749cc9e98e0d26b1a705
Anu Khambete <anukhambete@gmail.com>
2018-05-21 23:42:10 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,9 @@ module Players
     
     def move(board)
     #puts "Which cell would you like to pick?"
-    if @board.cells.all? {|
+    if board.cells.all? {|cell| cell == " "}
+      input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
+    else
       input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
     end
     
