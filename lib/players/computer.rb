f1e8c96e3cdf35c969e54d0e14eceed7c48a341a
Anu Khambete <anukhambete@gmail.com>
2018-05-21 23:47:13 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,19 @@
 module Players
   class Computer < Player
     
+    WIN_ARR=
+  [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+    ["1", "4", "7"],
+    [],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+    
+    
     def move(board)
     #puts "Which cell would you like to pick?"
       if board.cells.all? {|cell| cell == " "}
