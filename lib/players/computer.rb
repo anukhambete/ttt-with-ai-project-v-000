212bc631fedcedbdb3de6b6b31133b51e7915cca
Anu Khambete <anukhambete@gmail.com>
2018-05-22 00:25:04 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -27,6 +27,9 @@ module Players
         #if wa.include? (" xxxxxx")
         #wa.each do |element|
         #temp_array << element
+        #end
+        #end
+        #end
         input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
       end
     end
